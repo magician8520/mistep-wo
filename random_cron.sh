a77aec9809b7e132de3ecb271e48bcfa3ccd1974
magician8520 <34188062+magician8520@users.noreply.github.com>
2022-02-14 14:13:59 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-sed -i '/cron:/d' .github/workflows/mistep-wo.yml
+sed -i '/cron:/d' .github/workflows/baohuo.yml
 time=$(($RANDOM%60))" 0 * * "`date -d next-day +%w`
 echo $time
-sed -i '/schedule/a\    - cron: '$time'' .github/workflows/mistep-wo.yml
+sed -i '/schedule/a\    - cron: '$time'' .github/workflows/baohuo.yml
 
 exit 0
