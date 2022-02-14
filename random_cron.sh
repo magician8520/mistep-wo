87020544208387da01b00fcb411d5e46dc3bb6bc
magician8520 <34188062+magician8520@users.noreply.github.com>
2022-02-14 21:55:44 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -1,8 +1,15 @@
 #!/bin/zsh
 
 sed -i '/cron:/d' .github/workflows/baohuo.yml
-nm=`date -d next-month +%m`
-time=$(($RANDOM%60))" "$(($RANDOM%23))" 1 "`expr $nm + 0`" *"
+nexttime="date --date='50 day'"
+nm=`expr $nexttime "+%m" + 0`
+nd=`expr $nexttime "+%d" + 0`
+nH=`expr $nexttime "+%H" + 0`
+nM=`expr $nexttime "+%M" + 0`
+
+# nm=`date -d next-month +%m`
+# time=$(($RANDOM%60))" "$(($RANDOM%23))" 1 "`expr $nm + 0`" *"
+time=$nM" "$nH" "$nd" "$nm" *"
 echo $time
 sed -i '/schedule/a\    - cron: '$time'' .github/workflows/baohuo.yml
 
