0a096fa4aa2f4623a1b14a44c6141da753dce072
magician8520 <34188062+magician8520@users.noreply.github.com>
2022-02-14 21:09:05 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 sed -i '/cron:/d' .github/workflows/baohuo.yml
-time=$(($RANDOM%60))" 0 1 * *"
+time="0 0 1 * *"
 echo $time
 sed -i '/schedule/a\    - cron: '$time'' .github/workflows/baohuo.yml
 
